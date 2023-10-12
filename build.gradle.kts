58e1027187206fa35526193fb811ea67fb0459fc
Tom <mail@tomjuri.de>
2023-10-12 20:29:00 +0200

    add maven publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "1.9.0"
     id("cc.polyfrost.loom") version "0.10.0.5"
     id("dev.architectury.architectury-pack200") version "0.1.3"
+    id("maven-publish")
 }
 
 group = "dev.macrohq"
@@ -17,6 +18,7 @@ dependencies {
     forge("net.minecraftforge:forge:1.8.9-11.15.1.2318-1.8.9")
     compileOnly("org.spongepowered:mixin:0.8.5-SNAPSHOT")
     annotationProcessor("org.spongepowered:mixin:0.8.5-SNAPSHOT:processor")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
 }
 
 loom {
@@ -28,9 +30,24 @@ loom {
 }
 
 java {
+    withSourcesJar()
+    withJavadocJar()
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+    toolchain.languageVersion = JavaLanguageVersion.of(8)
 }
 
-java.toolchain.languageVersion = JavaLanguageVersion.of(8)
-kotlin.jvmToolchain(8)
+kotlin {
+    jvmToolchain(8)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "dev.macrohq"
+            artifactId = "macroframework"
+            version = project.version.toString()
+            from(components["java"])
+        }
+    }
+}
